f7feeadb0b3345765ac99494c35abbe143e0efef
houcine1amraoui <houcine1amraoui@gmail.com>
2025-11-13 10:28:12 +0100

    updated

---
+++ main.py
@@ -0,0 +1,3 @@
+# imports
+
+# run the functions here...
---
+++ utils.py
@@ -0,0 +1,54 @@
+def greedy(coins, amount):
+    # Sort coins descendingly
+    coins.sort(reverse=True)
+
+    result = []
+    remaining = amount
+    
+    for coin in coins:
+        while remaining >= coin:
+            remaining -= coin
+            result.append(coin)
+    
+    if remaining != 0:
+        print("No possible solution")
+        return -1
+    
+    return result
+
+def brute_force(coins, amount):
+    # Base cases
+    if amount == 0:
+        return 0
+    if amount < 0:
+        return float('inf')  # invalid path
+    
+    # Try each coin
+    min_coins = float('inf')
+
+    for coin in coins:
+        res = brute_force(coins, amount - coin)
+        if res != float('inf'):
+            min_coins = min(min_coins, 1 + res)
+
+    return min_coins
+
+def dynamic_programming(coins, amount, memo=None):
+    # modify the brute force function
+    # with memoization
+
+    # Base cases
+    if amount == 0:
+        return 0
+    if amount < 0:
+        return float('inf') # invalid path
+
+    # Try each coin
+    min_coins = float('inf')
+
+    for coin in coins:
+        res = dynamic_programming(coins, amount - coin, memo)
+        if res != float('inf'):
+            min_coins = min(min_coins, 1 + res)
+
+    return min_coins
